9edf7ae9a2cc36249c08b7bd8ff849739199a0d3
Michi2806 <michi2806@gmail.com>
2019-11-17 19:24:31 +0100

    Added timeout

--- src/bot.js
+++ src/bot.js
@@ -47,14 +47,6 @@ client.on("guildDelete", guild => {
 	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
-async function sendMessage(message, time) {
-	setTimeout(function () {
-        message.channel.send("DELAY DONE");
-    }, time);
-	message.channel.send("BEFORE DELAY");
-}
-
-
 client.on("message", async message => {
 	let serverData = storedData.servers[message.guild.id];
 	if (serverData == undefined) {
@@ -73,6 +65,12 @@ client.on("message", async message => {
 	// which is set in the configuration file.
 	if (message.content.indexOf(serverData.prefix) !== 0) return;
 
+	if (serverData.channel !== '' && serverData.channel !== undefined) {
+		if (message.channel.id !== serverData.channel) {
+			return;
+		}
+	}
+
 	// Here we separate our "command" name, and our "arguments" for the command. 
 	// e.g. if we have the message "+say Is this the real life?" , we'll get the following:
 	// command = say
@@ -82,8 +80,86 @@ client.on("message", async message => {
 
 	// Let's go with a few common example commands! Feel free to delete or change those.
 
-	if (command === "delay") {
-		sendMessage(message, args[0]);
+	if (command === "help") {
+		message.channel.send('Current Prefix: \'' + serverData.prefix + '\'\n' 
+							+ 'List of Commands: \n' 
+							+ '  ● help\n'
+							+ '  ● setPrefix [prefix] - Sets new Prefix\n'
+							+ '  ● limitChannel - Toggles channel lock\n'
+							+ '  ● timeout [@user] [time] - Sends user to afk channel for time sekonds\n'
+							+ '  ● ping\n')
+	}
+
+	if (command === "setprefix") {
+		if (args[0] === undefined) {
+			message.channel.send('Use setPrefix x to set the prefix to x');
+		} else {
+			serverData.prefix = args[0];
+			if (storedData.servers[message.guild.id] == undefined) {
+				storedData.servers[message.guild.id] = {};
+			}
+			storedData.servers[message.guild.id].prefix = args[0];
+			message.channel.send('Prefix has been set to \'' + args[0] + '\'');
+
+			fs.writeFile('data/data.json', JSON.stringify(storedData), function(err) {
+				console.log(err);
+			});
+		}
+		
+	}
+
+	if (command === "limitchannel") {
+		if (serverData.channel == undefined || serverData.channel == '') {
+			serverData.channel = message.channel.id;
+			if (storedData.servers[message.guild.id] == undefined) {
+				storedData.servers[message.guild.id] = {};
+			}
+			storedData.servers[message.guild.id].channel = message.channel.id;
+			message.channel.send('Bot commands have been restricted to this channel.');
+
+			fs.writeFile('data/data.json', JSON.stringify(storedData), function(err) {
+				console.log(err);
+			});
+		} else {
+			serverData.channel = '';
+			message.channel.send('Bot commands restrictions have been removed.');
+		}		
+	}
+
+	if (command === "timeout") {
+		let member;
+		let time;
+		if (args[0] === undefined) {
+			member = await message.guild.fetchMember(message.author.id);
+		} else {
+			if (args[0].indexOf('<@!') == 0) {
+				let id = args[0].slice(3, -1);
+				member = await message.guild.fetchMember(id);
+			}
+		}
+		if (member === undefined) {
+			message.channel.send('User is not connecte to voice chat.');
+			return;
+		}
+		if (member.voiceSessionID !== undefined) {
+		if (args[1] === undefined) {
+			time = 5000;
+		} else {
+			time = args[1] / 1000.0;
+		}
+
+		console.log(member.voiceChannel);
+			let channel = member.voiceChannel;	
+		
+			member.setVoiceChannel(member.guild.afkChannel);	
+			setTimeout(function () {
+				if (member.voiceSessionID !== undefined) {
+					member.setVoiceChannel(channel);
+				}
+			}, time);
+		} else {
+			message.channel.send('User is not connecte to voice chat.');
+		}		
 	}
 
 	if (command === "ping") {
@@ -91,80 +167,6 @@ client.on("message", async message => {
 		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
 		const m = await message.channel.send("Ping?");
 		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-	}
-
-	if (command === "say") {
-		// makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-		// To get the "message" itself we join the `args` back into a string with spaces: 
-		const sayMessage = args.join(" ");
-		// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-		message.delete().catch(O_o => { });
-		// And we get the bot to say the thing: 
-		message.channel.send(sayMessage);
-	}
-
-	if (command === "kick") {
-		// This command must be limited to mods and admins. In this example we just hardcode the role names.
-		// Please read on Array.some() to understand this bit: 
-		// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-		if (!message.member.roles.some(r => ["Administrator", "Moderator"].includes(r.name)))
-			return message.reply("Sorry, you don't have permissions to use this!");
-
-		// Let's first check if we have a member and if we can kick them!
-		// message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-		// We can also support getting the member by ID, which would be args[0]
-		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-		if (!member)
-			return message.reply("Please mention a valid member of this server");
-		if (!member.kickable)
-			return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-
-		// slice(1) removes the first part, which here should be the user mention or ID
-		// join(' ') takes all the various parts to make it a single string.
-		let reason = args.slice(1).join(' ');
-		if (!reason) reason = "No reason provided";
-
-		// Now, time for a swift kick in the nuts!
-		await member.kick(reason)
-			.catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-		message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-	}
-
-	if (command === "ban") {
-		// Most of this command is identical to kick, except that here we'll only let admins do it.
-		// In the real world mods could ban too, but this is just an example, right? ;)
-		if (!message.member.roles.some(r => ["Administrator"].includes(r.name)))
-			return message.reply("Sorry, you don't have permissions to use this!");
-
-		let member = message.mentions.members.first();
-		if (!member)
-			return message.reply("Please mention a valid member of this server");
-		if (!member.bannable)
-			return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-		let reason = args.slice(1).join(' ');
-		if (!reason) reason = "No reason provided";
-
-		await member.ban(reason)
-			.catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-		message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-	}
-
-	if (command === "purge") {
-		// This command removes all messages from all users in the channel, up to 100.
-
-		// get the delete count, as an actual number.
-		const deleteCount = parseInt(args[0], 10);
-
-		// Ooooh nice, combined conditions. <3
-		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-			return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
-		// So we get our messages, and delete them. Simple enough, right?
-		const fetched = await message.channel.fetchMessages({ limit: deleteCount });
-		message.channel.bulkDelete(fetched)
-			.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
 	}
 });
 
